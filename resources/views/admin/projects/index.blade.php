06a6537dd779dc0733af9ba285d3a911e86130f5
Leo9723 <pala9761@gmail.com>
2023-02-28 15:24:35 +0100

    first commit

---
+++ resources/views/admin/projects/index.blade.php
@@ -0,0 +1,35 @@
+@extends('layouts.admin')
+@section('content')
+
+
+<h1>Progetti</h1>
+
+<table class="table table-dark table-hover table-bordered">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Titolo</th>
+      <th scope="col">Descrizione</th>
+      <th scope="col">azioni</th>
+    </tr>
+  </thead>
+  <tbody>
+  @foreach($projects as $project)
+    <tr>
+      <th scope="row">{{ $project['id'] }}</th>
+      <td>{{ $project['title'] }}</td>
+      <td>{{ $project['description'] }}</td>
+      <td>
+        <button>V</button>
+        <button>M</button>
+        <button>E</button>
+      </td>
+    </tr>
+    @endforeach
+  </tbody>
+</table>
+
+<button>Aggiungi Progetto</button>
+
+
+@endsection
